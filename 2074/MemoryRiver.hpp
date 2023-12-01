5bf3ee486ea5d9c2579a04289373e2685d434930
Czar <hz.czar@sjtu.edu.cn>
2023-12-02 00:01:41 +0800

    Remove Debug line

--- 2074/MemoryRiver.hpp
+++ 2074/MemoryRiver.hpp
@@ -43,10 +43,8 @@ public:
   //检查是否是第一次打开
   bool IS_FIRST_OPEN() {
     file.open(file_name, std::ios::in | std::ios::out | std::ios::ate);
-    std::cout<<"!!\n";
     if(!file.is_open())
     {
-      std::cout<<"!!!\n";
       ofstream file(file_name);
       file.open(file_name, std::ios::in | std::ios::out | std::ios::ate);
     }
